46782859caa2e34bc08cde7be3e8456db0884e20
MusB4r <devmusbar@gmail.com>
2023-01-13 13:37:41 -0500

    Se sube el archivo

---
+++ pantalla.py
@@ -0,0 +1,11 @@
+import Calculadora
+import time
+resultado_suma = Calculadora.suma(2, 3)
+resultado_resta = Calculadora.resta(5, 2)
+resultado_multiplica = Calculadora.multiplica(4, 6)
+resultado_divide = Calculadora.divide(8, 2)
+
+print("Resultado de la suma: ", resultado_suma)
+print("Resultado de la resta: ", resultado_resta)
+print("Resultado de la multiplicación: ", resultado_multiplica)
+print("Resultado de la división: ", resultado_divide)
